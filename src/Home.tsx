1387f04a72924cf671a1be90ad9a56d450800dac
Lucas Guilherme <lguilherme8@gmail.com>
2022-04-05 17:08:02 -0300

    refactor of images folder and import

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,13 +2,14 @@ import { useContext } from 'react'
 import UserContext from './contexts/UserContext'
 import User from './components/User'
 import Form from './components/Form'
+import spaceSVG from './img/desenho.svg'
 
 const Home = () => {
   const { user } = useContext(UserContext)
 
   return (
     <div className="h-screen flex flex-col justify-center items-center bg-erie-black">
-      <img src="../assets/desenho.svg" alt="person with phone"
+      <img src={spaceSVG} alt="person with phone"
       className="absolute z-0 left-10 -bottom-10 pointer-events-none" />
       {user ? <User></User> : <Form></Form>}
     </div>
