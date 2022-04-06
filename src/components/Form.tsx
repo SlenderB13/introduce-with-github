a91ec0ed9baa6306ac07d8cd124f59ebe0ea410d
Lucas Guilherme <lguilherme8@gmail.com>
2022-04-05 23:25:21 -0300

    added responsive layout and avoid null requests

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -7,13 +7,15 @@ const Form = () => {
 
     const handleSubmit = (e: SyntheticEvent) => {
         e.preventDefault()
-        fetchData(inputValue)
+        if(inputValue) {
+          fetchData(inputValue)
+        }
     } 
     return (
-      <form className="h-1/2 md:w-4/5 lg:w-2/5 p-4 flex flex-col justify-evenly items-center text-center z-10"
+      <form className="h-1/2 w-full md:w-4/5 lg:w-3/5 2xl:w-2/5 p-4 flex flex-col justify-evenly items-center text-center z-10"
         onSubmit={handleSubmit}
       >
-        <h1 className="text-white font-sans sm:text-5xl">
+        <h1 className="text-white font-sans text-3xl sm:text-4xl md:text-5xl">
           Introduce yourself <br /> with <span className='font-bold tracking-wider'>Github</span>
           </h1>
         <div className="flex flex-col items-center justify-evenly h-1/2 w-8/12">
@@ -23,9 +25,12 @@ const Form = () => {
             value={inputValue} 
             onChange={(e) => setInputValue(e.target.value)} 
             placeholder="slenderb13"
-            className="h-12 rounded-md indent-3 w-full" 
+            className="h-12 rounded-md text-center w-full" 
           />
-          <input className="text-erie-black tracking-wider font-sans h-12 w-full rounded-md bg-marigold" 
+          <input className="text-erie-black tracking-wider 
+            font-sans h-12 w-full 
+            rounded-md bg-marigold 
+            hover:brightness-90 hover:cursor-pointer" 
           type="submit" 
           value="THAT'S ME" />
         </div>
--- src/components/User.tsx
+++ src/components/User.tsx
@@ -15,12 +15,12 @@ const User= () => {
 
     return (
         <div ref={userWrapperRef} className="h-3/5 md:w-4/5 flex flex-col items-center justify-around z-10">
-            <figure className="w-85 h-85 rounded-full border-solid border-4 border-marigold">
-                <img className="w-80 h-80 rounded-full" src={user?.avatar_url} alt="user avatar" />
+            <figure className="w-45 h-45 md:w-[53] md:h-[53] lg:w-[61] lg:h-[61] rounded-full border-solid border-4 border-marigold">
+                <img className="w-44 h-44 md:w-52 md:h-52 lg:w-60 lg:h-60 rounded-full" src={user?.avatar_url} alt="user avatar" />
             </figure>
             <h1 className="text-white font-bold tracking-wider text-5xl">{user?.name}</h1>
-            <h2 className="text-white text-2xl">{user?.location}</h2>
-            <h3 className="text-white lg:text-3xl md:text-2xl">{user?.bio}</h3>
+            <h2 className="text-white text-2xl">{user?.location ? user.location : 'User do not have a location'}</h2>
+            <h3 className="text-white text-2xl max-w-sm lg:max-w-4xl lg:text-3xl">{ user?.bio ? user.bio : 'User do not have a description'}</h3>
         </div>
     )
 }
