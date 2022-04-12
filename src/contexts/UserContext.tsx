71412991e495e48230c5afc3a1363ee6adfe4d4d
Lucas Guilherme <lguilherme8@gmail.com>
2022-04-12 16:05:27 -0300

    added restart option and credits

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -4,6 +4,7 @@ import User from "../types/User"
 export type UserContextType = {
     user: User | null,
     fetchData: (url: string) => Promise<void>
+    clearUser: () => void
 }
 
 const UserContext = createContext<UserContextType>(null!)
